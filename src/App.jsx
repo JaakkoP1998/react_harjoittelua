9445d17c8905849276749cd10b5b2d0f51f30515
Jaakko Palm <jaakkop1998@gmail.com>
2025-09-01 15:17:22 +0300

    Perusteiden kertaus

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Pekka" />
+    </div>
+  )
+}
+
+export default App
