961949e963ed0e7c0fa7184cf975619f62fb5212
Jaakko Palm <jaakkop1998@gmail.com>
2025-09-02 14:27:32 +0300

    Värin vaihtelun testaamista

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}</p>
-    </div>
-  )
-}
+import {useState,useEffect} from 'react';
 
 const App = () => {
+  
+  const [category, setCategory] = useState('');
+
+  // "category" -nimisen select elementin käsittelijä
+  const handleColorChange = (category) => {
+     setCategory(category);
+     console.log(category);
+  }
+
+  // H2-elementti saa taustavärinsä select-elementin valinnoista.
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Pekka" />
+      <div>
+        <h1>Greetings</h1>
+        <h2 style={{ backgroundColor: category || 'transparent', padding: '10px' }} >Box for testing</h2>
+      </div>
+      <select name="category" 
+        value={category} onChange={event => handleColorChange(event.target.value)}>
+            <option value="">Select Color</option>
+            <option value="red">Red</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+        </select>
     </div>
   )
 }
