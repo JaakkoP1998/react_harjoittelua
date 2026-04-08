ea2707327d8f60686aee14cf45760221a2db3887
Jaakko Palm <jaakkop1998@gmail.com>
2026-04-08 19:42:23 +0300

    Added localstorage for saving login information and logout feature for users

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import loginService from "../services/login"
 import commentService from "../services/comments"
 
@@ -9,6 +9,16 @@ const LoginForm = () => {
     const [password, setPassword] = useState('') 
     // Variable for logged in user.
     const [user, setUser] = useState(null)
+    
+    // Check if user is already found from local storage.
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedUser')
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+            commentService.setToken(user.token)
+        }
+    }, [])
 
     // Handler for submitting form. 
     const handleLogin = async event => {
@@ -18,13 +28,15 @@ const LoginForm = () => {
         try {
             // Send credentials to loginService
             const loggedUser = await loginService.login({ username, password })
-            console.log("LOGIN RESPONSE:", loggedUser)
+
+            // Save user to local storage.
+            window.localStorage.setItem(
+                'loggedUser', JSON.stringify(loggedUser)
+            ) 
 
             commentService.setToken(loggedUser.token)
             // Set logged in user and nullify login form.
             setUser(loggedUser)
-            // Simple alert window, for showing if login was successfull or not.
-            alert(`Logged in as ${username}`)
             setUsername('')
             setPassword('')
         } catch (error) {
@@ -32,7 +44,16 @@ const LoginForm = () => {
             // console.log('wrong credentials')
             console.error(error)
             alert("Wrong username or credentials.")
+        }
     }
+
+    // Handler for logging out.
+    const handleLogOut = (event) => {
+        event.preventDefault()
+        // Remove user from local storage.
+        window.localStorage.removeItem('loggedUser')
+        // Refresh page so that the logout is successfull.
+        window.location.reload()
     }
 
     return (
@@ -59,9 +80,10 @@ const LoginForm = () => {
                         />
                     </label>
                 </div>
-                <button type="submit"> login </button>
+                <button type="submit"> Login </button>
             </form>
-            {/* Usernames conditional rendering */}
+            <button onClick={handleLogOut}> Logout </button>
+            {/* Conditional rendering for showing logged in user's name.*/}
             {user && (
                 <div>
                     <p>{user.name} logged in</p>
